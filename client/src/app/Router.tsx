daae42384c49dd39320b4ee104dc3ec0b74fb86b
KevinFCarrillo <fcarrillo@ideeasoluciones.com>
2026-03-10 10:01:22 -0500

    homepage
    primer bosquejo del homepage

--- client/src/app/Router.tsx
+++ client/src/app/Router.tsx
@@ -2,13 +2,14 @@ import Layout from "@/shared/components/Layout";
 import NotFound from "@/app/not-found";
 import { Switch, Route } from "wouter";
 import ValidateClients from "@/features/validateClients/ValidateClientsPage";
+import HomePage from "@/features/HomePage/HomePage";
 
 function Router() {
   return (
     <Layout>
       <Switch>
         <Route path="/validar" component={ValidateClients} />
-        <Route path="/" component={NotFound} />
+        <Route path="/" component={HomePage} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
