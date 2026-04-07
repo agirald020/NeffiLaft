16b659cb782ab3743e2d476436e433cad0f1d87d
KevinFCarrillo <fcarrillo@ideeasoluciones.com>
2026-04-07 17:58:59 -0500

    Ajuste logo
    Se actualiza el logo de accion por el nuevo

--- client/src/features/HomePage/HomePage.tsx
+++ client/src/features/HomePage/HomePage.tsx
@@ -5,19 +5,21 @@ interface HomePageProps {
 }
 
 const HomePage: FunctionComponent<HomePageProps> = () => {
-  return (<div className="h-full w-full flex items-center justify-center bg-gray-50">
-    <Card className="w-full max-w-md mx-4">
-      <CardContent className="pt-6">
-        <div className="flex mb-4 gap-2">
-          <h1 className="text-2xl font-bold text-red-500">BIENVENIDO</h1>
-        </div>
+  return (
+    <div className="h-full w-full flex items-center justify-center bg-gray-50">
+      <Card className="w-full max-w-md mx-4">
+        <CardContent className="pt-6">
+          <div className="flex mb-4 gap-2">
+            <h1 className="text-2xl font-bold text-red-500">BIENVENIDO</h1>
+          </div>
 
-        <p className="mt-4 text-sm text-gray-600">
-          En este sitio podrá consultar el estado de sus clientes, para ello dirijase a la seccion de validar clientes.
-        </p>
-      </CardContent>
-    </Card>
-  </div>);
+          <p className="mt-4 text-sm text-gray-600">
+            En este sitio podrá consultar el estado de sus clientes, para ello dirijase a la seccion de validar clientes.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default HomePage;
